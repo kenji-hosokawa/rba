fd79a4070993f18209173ddd2ba02ed3b1891e2a
Naohiro Nishiguchi <nnishiguchi@jp.adit-jv.com>
2020-07-13 09:13:15 +0900

    src:core:expression: completed translation of RBAIsEqualToOperator, RBAIsGreaterThanEqualOperator and RBAIsGreaterThanOperator

--- src/core/expression/RBAIsEqualToOperatorMaker.hpp
+++ src/core/expression/RBAIsEqualToOperatorMaker.hpp
@@ -1,7 +1,21 @@
+/**
+ * Copyright (c) 2019 DENSO CORPORATION.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /// @file  RBAIsEqualToOperatorMaker.hpp
-/// @brief IsEqualToOperatorオブジェクト生成クラスヘッダファイル
-///
-/// Copyright (c) 2019 DENSO CORPORATION. All rights reserved.
+/// @brief IsEqualToOperator object generation file
 
 #ifndef RBAISEQUALTOOPERATORMAKER_HPP
 #define RBAISEQUALTOOPERATORMAKER_HPP
@@ -11,7 +25,7 @@
 namespace rba
 {
 
-/// @brief IsEqualToOperatorオブジェクト生成クラス
+/// @brief IsEqualToOperator object generation class
 class RBAIsEqualToOperatorMaker : public RBAOperatorMaker
 {
 public:
@@ -23,9 +37,9 @@ public:
   virtual ~RBAIsEqualToOperatorMaker()=default;
 
 protected:
-  /// @brief 空のインスタンス生成
-  /// @details 派生クラスでunique_ptrの空のインスタンスを生成する。
-  /// @return インスタンスのunique_ptr
+  /// @brief generate an empty instance
+  /// @details generate an empty instance of unique_ptr in derived class
+  /// @return unique_ptr for instance
   std::unique_ptr<RBAModelElement> createInstance(const std::string& name="") override;
 
 };
