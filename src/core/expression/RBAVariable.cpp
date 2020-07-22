58eed53e3ebfc759b4c7de1fa907a95dfa331535
Naohiro Nishiguchi <nnishiguchi@jp.adit-jv.com>
2020-07-22 12:02:02 +0900

    src:core:expression translated RBAPropertyOperator.hpp, RBAValueExpression.hpp and RBAVariable*

--- src/core/expression/RBAVariable.cpp
+++ src/core/expression/RBAVariable.cpp
@@ -1,4 +1,22 @@
-/// 変数クラス定義ファイル
+/**
+ * Copyright (c) 2019 DENSO CORPORATION.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Variable class defintion
+ */
 
 #include "RBAVariable.hpp"
 #include "RBAModelElementType.hpp"
@@ -38,14 +56,16 @@ RBAVariable::clearRuleObj()
 RBAConstraintInfo*
 RBAVariable::createConstraintInfo()
 {
-  // Let式がFor-Allなどのラムダ式の中に書かれた場合、
-  // For-Allのxなどのvaliableが変化するたびに、Let式の展開結果も変化する。
-  // Let式がFor-Allの中に掛かれていたとしても、
-  // しかし、そのvaliableオブジェクトは1つしかないよって、
-  // constraintInfoをクリアして使い回すことは出来ず、
-  // Let式を評価するたびに、make_sharedする必要がある。
-  // なお、Let式のConstraintInfoは、Let式の定義を参照しているRBAObjectReference評価時に
-  // そのRBAObjectReferenceのConstraintInfoの下に所有権が移る。
+  // When the "Let" expression is written in a lambda expression such as 
+  // "For-All", the expansion result of the "Let" expression also changes 
+  // each time the variable of "For-All" changes. Even if the Let expression 
+  // is written in "For-All", there is only one valiable object.
+  // Therefore, "constraintInfo" cannot be cleared and reused, and it is 
+  // necessary to do "make_shared" each time the "Let" expression is evaluated.
+  // The ownership of the "ConstraintInfo" of the "Let" expression is 
+  // transferred to the "ConstraintInfo" of the "RBAObjectReference" 
+  // when the RBAObjectReference that refers to the definition of the Let 
+  // expression is evaluated.
   constraintInfo_ = std::make_shared<RBAConstraintInfo>();
   return constraintInfo_.get();
 }
--- src/core/expression/RBAVariableMaker.hpp
+++ src/core/expression/RBAVariableMaker.hpp
@@ -1,7 +1,21 @@
-/// @file  RBAVariableMaker.hpp
-/// @brief 変数オブジェクト生成クラスヘッダファイル
-///
-/// Copyright (c) 2019 DENSO CORPORATION. All rights reserved.
+/**
+ * Copyright (c) 2019 DENSO CORPORATION.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/// @file  RBAVariableMaker.cpp
+/// @brief Variable object generator class header
 
 #ifndef RBAVARIABLEMAKER_HPP
 #define RBAVARIABLEMAKER_HPP
@@ -11,7 +25,7 @@
 namespace rba
 {
 
-/// @brief 変数オブジェクト生成クラス
+/// @brief Variable object generator class
 class RBAVariableMaker : public RBAModelElementMaker
 {
 public:
@@ -23,26 +37,27 @@ public:
   virtual ~RBAVariableMaker()=default;
 
 public:
-  /// @brief ModelElementオブジェクト生成
-  /// @details ModelElementオブジェクトを生成して、unique_ptrをmodelに登録する。
-  /// @param[in] jsonElem ModelElementのJSONエレメント
-  /// @param[in] model 生成したオブジェクトを保存するモデル
-  /// @param[in,out] owner 親オブジェクト（未使用）
-  /// @return ModelElementオブジェクト
+  /// @brief Create ModelElement object
+  /// @details Create ModelElement object and register unique_ptr to model.
+  /// @param[in] jsonElem JSON element of ModelElement
+  /// @param[in] model The model to store created object
+  /// @param[in,out] owner Parent object (Not in use)
+  /// @return ModelElement object
   RBAModelElement* create(const RBAJsonElement* jsonElem,
                           RBAModelImpl* model,
                           RBAModelElement* owner=nullptr) override;
 
 protected:
-  /// @brief 空のインスタンス生成
-  /// @details 派生クラスでunique_ptrの空のインスタンスを生成する。
-  /// @return インスタンスのunique_ptr
+  /// @brief generate an empty instance
+  /// @details generate an empty instance of unique_ptr in derived class
+  /// @return unique_ptr for instance
   std::unique_ptr<RBAModelElement> createInstance(const std::string& name="") override;
 
-  /// @brief 派生クラスごとにモデルエレメントの属性をセットする
-  /// @param[in] jsonElem ModelElementのJSONエレメント
-  /// @param[in] model 生成したオブジェクトを保存するモデル
-  /// @param[in,out] owner 親オブジェクト（未使用）
+  /// @brief Set attributes of model element for each derived class
+  /// @param[in] jsonElem JSON element of ModelElement
+  /// @param[in] model The model to store created object
+  /// @param[in,out] owner Parent object (Not in use)
+  /// @return ModelElement object
   RBAModelElement* setProperty(RBAModelElement* element,
                                const RBAJsonElement* jsonElem,
                                RBAModelImpl* model,
