a594ae66150593864eb164c1d7429b286548a64d
Naohiro Nishiguchi <nnishiguchi@jp.adit-jv.com>
2020-07-24 16:23:31 +0900

    src:core:model: translated RBAAbstract* and RBAAllocatable*

--- src/core/model/RBAAbstractAllocatableMaker.cpp
+++ src/core/model/RBAAbstractAllocatableMaker.cpp
@@ -1,7 +1,21 @@
+/**
+ * Copyright (c) 2019 DENSO CORPORATION.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /// @file  RBAAbstractAllocatableMaker.cpp
-/// @brief 抽象アロケータブル生成クラス定義ファイル
-///
-/// Copyright (c) 2019 DENSO CORPORATION. All rights reserved.
+/// @brief AbstractAllocatable generator class implementation
 
 #include "RBAAbstractAllocatableMaker.hpp"
 #include "RBAAbstractAllocatable.hpp"
@@ -28,8 +42,9 @@ RBAAbstractAllocatableMaker::setProperty(RBAModelElement* element,
   
   const auto abstAllo = dynamic_cast<RBAAbstractAllocatable*>(element);
 
-  // 現在、モデル情報としてRBAAbstractAllocatableMakerでセットするメンバはないが、
-  // 今後追加される可能性があるのでこの処理は残しておく。
+  // Currently, there is no member set by RBAAbstractAllocatableMaker 
+  // as model information, but this process is implemented 
+  // because it may be added in the future.
 
   return abstAllo;
 }
